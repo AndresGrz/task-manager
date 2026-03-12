2d3e1aee6a09ffc20722a575eeff7c63d775ce4f
Andres Garza <andresgarzadavila1996@gmail.com>
2026-03-12 17:46:19 -0600

    Agregar comando para iniciar la base de datos

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
+
+// To start MongoDB, run the following command in the terminal from: C:\Users\andres.garza\mongodb\bin
+// .\mongod.exe --dbpath="C:\Users\andres.garza\mongodb-data"
